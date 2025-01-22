ae9cfd42c872302e36895332197eb9c11cffca7c
Alsha Behera <alshabehera7@gmail.com>
2025-01-22 18:20:02 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
-import Product from "./components/Product";
+import Home from "components/Home";
+import PageNotFound from "components/PageNotFound";
+import Product from "components/Product";
+import { Route, Switch, NavLink } from "react-router-dom";
 
-const App = () => <Product />;
+const App = () => (
+  <>
+    <div className="mx-4 flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route component={Product} path="/product" />
+      <Route component={Home} path="/" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
+
 export default App;
