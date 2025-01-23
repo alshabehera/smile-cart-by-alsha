5d673356818216892496d4026d65cdc3997f8600
Alsha Behera <alshabehera7@gmail.com>
2025-01-23 06:35:22 +0530

    Implemented search functionality to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 
 import productsApi from "apis/products";
 import { Header, PageLoader } from "components/commons";
+import { Search } from "neetoicons";
+import { Input, NoData } from "neetoui";
+import { isEmpty } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchKey, setSearchKey] = useState("");
 
   const fetchProducts = async () => {
     try {
-      const { products } = await productsApi.fetch();
-      setProducts(products);
+      const data = await productsApi.fetch({ searchTerm: searchKey });
+      setProducts(data.products);
     } catch (error) {
       console.log("An error occurred:", error);
     } finally {
@@ -22,22 +26,38 @@ const ProductList = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [searchKey]);
 
   if (isLoading) {
     return <PageLoader />;
   }
 
   return (
-    <div className="flex flex-col">
+    <div className="flex h-screen flex-col">
       <div className="m-2">
-        <Header shouldShowBackButton={false} title="Smile Cart" />
+        <Header
+          shouldShowBackButton={false}
+          title="Smile Cart"
+          actionBlock={
+            <Input
+              placeholder="Search products"
+              prefix={<Search />}
+              type="search"
+              value={searchKey}
+              onChange={event => setSearchKey(event.target.value)}
+            />
+          }
+        />
       </div>
-      <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
-        {products.map(product => (
-          <ProductListItem key={product.slug} {...product} />
-        ))}
-      </div>
+      {isEmpty(products) ? (
+        <NoData className="h-full w-full" title="No products to show" />
+      ) : (
+        <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
+          {products.map(product => (
+            <ProductListItem key={product.slug} {...product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
