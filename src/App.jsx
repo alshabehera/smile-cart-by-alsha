65a1d3535938377f5964ba70f948b26ebfc7fd49
Alsha Behera <alshabehera7@gmail.com>
2025-01-24 18:09:14 +0530

    Integrated Formik form into the checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Cart from "components/Cart";
+import Checkout from "components/Checkout";
 import { PageNotFound } from "components/commons";
 import Product from "components/Product";
 import ProductList from "components/ProductList";
@@ -12,6 +13,7 @@ const App = () => (
     <Route exact component={ProductList} path={routes.products.index} />
     <Redirect exact from={routes.root} to={routes.products.index} />
     <Route exact component={Cart} path={routes.cart} />
+    <Route exact component={Checkout} path={routes.checkout} />
     <Route component={PageNotFound} path="*" />
   </Switch>
 );
